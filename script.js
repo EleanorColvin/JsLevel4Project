ecd77f136ffc5a6d60e9dfa4091b00e62b746442
EleanorColvin <99199758+EleanorColvin@users.noreply.github.com>
2023-01-07 14:35:47 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -9,25 +9,46 @@ function initialize() {
     input = document.getElementById("newItem");
     selected = document.getElementById("movingItem");
     row = document.getElementById(selected.value);
+    del = document.getElementById("deletedItem");
+    addMess = document.getElementById("addMessage");
 }
 function addItem() {
-    length++;
+    var rep = false;
     let table = document.getElementById("myTable");
-    let newRow = myTable.insertRow(-1);
-    newRow.id = input.value;
-    let numCell = newRow.insertCell(0);
-    numCell.innerHTML = length + ")";
-    let newCell = newRow.insertCell(1);
-    newCell.innerHTML = input.value;
-    rows++;
+    for (var i = 0, row; row = table.rows[i]; i++) {
+        if(table.rows[i].id == input.value)
+        {
+            rep = true
+            console.log("you can't do that! <3");
+            addMess.innerHTML = input.value + " is already on your list!";
+            break;
+        }
+        else
+        {
+            rep = false;
+            addMess.innerHTML = "";
+        }
+    }
+    if(rep == false){
+        length++;
+        let newRow = myTable.insertRow(-1);
+        newRow.id = input.value;
+        let numCell = newRow.insertCell(0);
+        numCell.innerHTML = length + ")";
+        let newCell = newRow.insertCell(1);
+        newCell.innerHTML = input.value;
+        rows++;
+    }
+    document.getElementById("newItem").value = "";
 }
 function removeItem() {
     table = document.getElementById("myTable");
-    row = document.getElementById(input.value);
+    row = table.rows[del.value - 1];
     row.parentNode.removeChild(row);
     length--;
     rows--;
     setNums();
+    document.getElementById("deletedItem").value = "";
 }
 function setNums() {
     var table = document.getElementById("myTable");
@@ -40,7 +61,7 @@ function setNums() {
 }
 function deselect() {
     for (i = 0; i < rows; i++) {
-        table.rows[i].style.backgroundColor = "white";
+        table.rows[i].style.backgroundColor = "rgb(194, 231, 249)";
     }
     selId = "";
     sel = null;
@@ -52,7 +73,7 @@ function deselect() {
 function checkIfSelected() {
     table = document.getElementById("myTable");
     selected = document.getElementById("movingItem").value;
-    selected --;
+    selected--;
     if (!toString(selected.value) == "") {
         select = true;
         table.rows[selected].style.backgroundColor = "yellow";
@@ -69,7 +90,7 @@ function moveUp() {
         table = document.getElementById("myTable");
         row = sel;
         table.rows[selidx - 1].style.backgroundColor = "yellow";
-        table.rows[selidx].style.backgroundColor = "white";
+        table.rows[selidx].style.backgroundColor = "rgb(194, 231, 249)";
         s = selidx;
         idx2 = selidx - 1;
         moveRows(s, idx2);
@@ -85,11 +106,10 @@ function moveRows(idx1, idx2) {
     table.rows[idx2].id = tempid;
     sel = table.rows[idx2];
     selId = table.rows[idx2].id;
-    if(idx1 > idx2)
-    {
+    if (idx1 > idx2) {
         selidx--;
     }
-    else{
+    else {
         selidx++;
     }
     setNums();
@@ -104,7 +124,7 @@ function moveDown() {
         table = document.getElementById("myTable");
         row = sel;
         table.rows[selidx + 1].style.backgroundColor = "yellow";
-        table.rows[selidx].style.backgroundColor = "white";
+        table.rows[selidx].style.backgroundColor = "rgb(194, 231, 249)";
         s = selidx;
         idx2 = selidx + 1;
         moveRows(s, idx2);
